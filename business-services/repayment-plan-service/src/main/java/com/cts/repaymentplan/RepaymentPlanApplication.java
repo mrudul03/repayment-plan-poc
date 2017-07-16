bffb2eb158ccf37f1331e6807a5bfcd6b5d5b623
mrudul03 <mrudul.palvankar@gmail.com>
2017-07-16 11:35:34 +0530

    added java

---
+++ business-services/repayment-plan-service/src/main/java/com/cts/repaymentplan/RepaymentPlanApplication.java
@@ -0,0 +1,12 @@
+package com.cts.repaymentplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepaymentPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepaymentPlanApplication.class, args);
+    }
+}
