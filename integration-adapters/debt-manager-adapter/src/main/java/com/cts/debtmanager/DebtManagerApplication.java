1a3a2c9fdbe2fd2209fee080ead28cea3687e20d
mrudul03 <mrudul.palvankar@gmail.com>
2017-07-16 11:23:12 +0530

    added java

---
+++ integration-adapters/debt-manager-adapter/src/main/java/com/cts/debtmanager/DebtManagerApplication.java
@@ -0,0 +1,12 @@
+package com.cts.debtmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtManagerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DebtManagerApplication.class, args);
+    }
+}
